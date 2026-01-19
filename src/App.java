0a8f7defe865e63769d2f23d5753a385c5b63da5
Arthur Silva <arthurcom898estudos@gmail.com>
2026-01-19 11:26:13 -0300

    add items to carrinho and calculate total

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Sejam Bem-Vindos(as) a Lanchonete !");
+
+        Item item1 = new Item("Hambúrguer", 15.0, 1);
+        Item item2 = new Item("Batata Frita", 7.5, 2);
+        Item item3 = new Item("Refrigerante", 5.0, 3);
+        Item item4 = new Item("Milkshake", 10.0, 4);
+
+        Carrinho c = new Carrinho();
+
+        c.adicionarItem(item1);
+        c.adicionarItem(item2);
+        c.adicionarItem(item3);
+        c.adicionarItem(item4);
+
+        c.calcularPreco();
     }
 }
